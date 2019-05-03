5cc27d321fb4f3b10afc2239f9ff89570edbc956
Pownthep <pownthepofficial@gmail.com>
2019-05-03 23:01:36 +0700

    first commit

---
+++ build-script.js
@@ -0,0 +1,21 @@
+const fs = require('fs-extra');
+const concat = require('concat');    
+
+(async function build() {
+
+    const files =[
+        './dist/runtime.js',
+        './dist/polyfills.js',
+        './dist/scripts.js',
+        './dist/main.js'
+    ]
+    
+    await fs.ensureDir('elements');
+    
+    await concat(files, 'elements/elements.js');
+    await fs.copyFile(
+        './dist/styles.css',
+        'elements/styles.css'
+      );
+    console.info('Elements created successfully!');
+})();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
+// Import the components
 import { AppComponent } from './app.component';
+
+// Insert all the components you want to create as custom Elements into this array
+const components = [
+  AppComponent,
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    ...components,
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: components
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const elements: any[] = [
+      
+      //[Component name, 'selector name']
+      [AppComponent, 'app-comp']
+      
+    ];
+
+    for (const [component, name] of elements) {
+      const el = createCustomElement(component, { injector: this.injector });
+      customElements.define(name, el);
+    }
+  }
+
+}
